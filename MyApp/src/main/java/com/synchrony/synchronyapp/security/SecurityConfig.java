8efc7288e3f5daaffda79e157b8768b9af702817
Jahnabi Sharma <jahnabisharma@Mac.lan>
2025-05-06 10:51:56 +0530

    spring security added

---
+++ MyApp/src/main/java/com/synchrony/synchronyapp/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.synchrony.synchronyapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+				.build();
+		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+}
